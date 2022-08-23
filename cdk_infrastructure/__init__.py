71b542152e95889f8cac7434cf2633a1767c4911
Eugene <eugeneh101@DESKTOP-6EJMNGA.localdomain>
2022-08-23 12:59:01 -0700

    added ability to start exactly at the beginning of each 2 second interval

--- cdk_infrastructure/__init__.py
+++ cdk_infrastructure/__init__.py
@@ -1,4 +1,5 @@
-from constructs import Construct
+import typing
+
 from aws_cdk import (
     # BundlingOptions,
     Duration,
@@ -14,23 +15,37 @@ from aws_cdk import (
     aws_stepfunctions as sfn,
     aws_stepfunctions_tasks as sfn_tasks,
 )
+from constructs import Construct
 
 
 class OracleStack(Stack):  # later move code into constructs.py
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: Construct,
+        construct_id: str,
+        environment: typing.Optional[dict] = None,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        environment = {} if environment is None else environment
 
         # Secrets Manager
         self.secret = secretsmanager.Secret(
             self,
             "MyFavoriteSecret",
             # in real life, you would not put the secret in plaintext in CDK
-            secret_object_value={"my_secret": SecretValue.unsafe_plain_text("SUPER_SECRET_VALUE")},
+            secret_object_value={
+                "my_secret": SecretValue.unsafe_plain_text("SUPER_SECRET_VALUE")
+            },
         )
 
         # DynamoDB table
-        self.dynamodb_table = dynamodb.Table(self, "RandomIntegers",
-            partition_key=dynamodb.Attribute(name="datetime_utc", type=dynamodb.AttributeType.STRING),
+        self.dynamodb_table = dynamodb.Table(
+            self,
+            "RandomIntegers",
+            partition_key=dynamodb.Attribute(
+                name="datetime_utc", type=dynamodb.AttributeType.STRING
+            ),
             # CDK wil not automatically deleted DynamoDB during `cdk destroy`
             # (as DynamoDB is a stateful resource) unless explicitly specified by the following line
             removal_policy=RemovalPolicy.DESTROY,
@@ -44,12 +59,12 @@ class OracleStack(Stack):  # later move code into constructs.py
             schedule=events.Schedule.rate(Duration.minutes(1)),
         )
         # Lambdas
-        self.get_next_minute_lambda = _lambda.Function(
+        self.get_next_minute_and_invocation_times_lambda = _lambda.Function(
             self,
             "NextMinuteLambda",
             runtime=_lambda.Runtime.PYTHON_3_9,
             code=_lambda.Code.from_asset(
-                path="source/get_next_minute_lambda",
+                path="source/get_next_minute_and_invocation_times_lambda",
                 exclude=[".venv/*"],  # exclude virtualenv
             ),
             handler="handler.lambda_handler",
@@ -81,18 +96,24 @@ class OracleStack(Stack):  # later move code into constructs.py
         )
 
         # build Step Function definition
-        get_next_minute = sfn_tasks.LambdaInvoke(
+        get_next_minute_and_invocation_times = sfn_tasks.LambdaInvoke(
             self,
-            "get_next_minute",
-            lambda_function=self.get_next_minute_lambda,
+            "get_next_minute_and_invocation_times",
+            lambda_function=self.get_next_minute_and_invocation_times_lambda,
             payload=sfn.TaskInput.from_text("null"),
             payload_response_only=True,  # don't want Lambda invocation metadata
         )
-        sleep_to_next_minute = sfn.Wait(self, "sleep_to_next_minute", time=sfn.WaitTime.timestamp_path("$"))
-        initialize_sleeps = sfn.Pass(self, "initialize_sleeps", result=sfn.Result.from_array([2] * 30))  ### hard coded
-        for_loop = sfn.Map(self, id="for loop", items_path="$", max_concurrency=1)
+        sleep_to_next_minute = sfn.Wait(
+            self, "sleep_to_next_minute", time=sfn.WaitTime.timestamp_path("$.next_minute")
+        )
+        # initialize_sleeps = sfn.Pass(
+        #     self, "initialize_sleeps", result=sfn.Result.from_array([2] * 30)
+        # )  ### hard coded
+        for_loop = sfn.Map(self, id="for loop", items_path="$.invocation_times", max_concurrency=1)
 
-        sleep_seconds = sfn.Wait(self, "sleep_seconds", time=sfn.WaitTime.seconds_path("$"))
+        sleep_seconds = sfn.Wait(
+            self, "sleep_seconds", time=sfn.WaitTime.timestamp_path("$")
+        )
         update_database = sfn_tasks.LambdaInvoke(
             self,
             "update_database",
@@ -102,19 +123,32 @@ class OracleStack(Stack):  # later move code into constructs.py
         )
         map_state_tasks = sfn.Chain.start(sleep_seconds).next(update_database)
         for_loop.iterator(map_state_tasks)
-        sfn_definition = get_next_minute.next(sleep_to_next_minute).next(initialize_sleeps).next(for_loop)
-        self.state_machine = sfn.StateMachine(self, "RecurrentInvocations", definition=sfn_definition)
+        sfn_definition = (
+            get_next_minute_and_invocation_times.next(sleep_to_next_minute)
+            # .next(initialize_sleeps)
+            .next(for_loop)
+        )
+        self.state_machine = sfn.StateMachine(
+            self, "RecurrentInvocations", definition=sfn_definition
+        )
 
         # dependencies: lambda layer, environment variable, permissions, Eventbridge rule target
         powertools_layer = _lambda.LayerVersion.from_layer_version_arn(
             self,
             "aws_lambda_powertools",
-            layer_version_arn="arn:aws:lambda:us-east-1:017000801446:layer:AWSLambdaPowertoolsPython:29",  # might consider getting latest layer
+            layer_version_arn=(
+                f"arn:aws:lambda:{environment['AWS_REGION']}:"
+                "017000801446:layer:AWSLambdaPowertoolsPython:29"  # might consider getting latest layer
+            ),
         )
-        self.get_next_minute_lambda.add_layers(powertools_layer)
+        self.get_next_minute_and_invocation_times_lambda.add_layers(powertools_layer)
         self.update_db_lambda.add_layers(powertools_layer)
-        self.update_db_lambda.add_environment(key="SECRET_NAME", value=self.secret.secret_name)
-        self.update_db_lambda.add_environment(key="DYNAMODB_TABLE", value=self.dynamodb_table.table_name)
+        self.update_db_lambda.add_environment(
+            key="SECRET_NAME", value=self.secret.secret_name
+        )
+        self.update_db_lambda.add_environment(
+            key="DYNAMODB_TABLE", value=self.dynamodb_table.table_name
+        )
         self.dynamodb_table.grant_read_write_data(self.update_db_lambda)
         self.secret.grant_read(self.update_db_lambda.role)
         self.eventbridge_minute_scheduled_event.add_target(
@@ -124,6 +158,3 @@ class OracleStack(Stack):  # later move code into constructs.py
                 # dead_letter_queue=dlq,  # might consider for high availability
             )
         )
-
-
-
