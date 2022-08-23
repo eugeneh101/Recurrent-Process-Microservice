601e538686f190437ce949434bd4371330b4e258
Eugene <eugeneh101@DESKTOP-6EJMNGA.localdomain>
2022-08-22 22:49:20 -0700

    added type annotation

--- source/update_database_lambda/handler.py
+++ source/update_database_lambda/handler.py
@@ -12,7 +12,7 @@ dynamodb_table = boto3.resource("dynamodb").Table(os.environ["DYNAMODB_TABLE"])
 ssm = boto3.client("secretsmanager")
 SECRET = json.loads(ssm.get_secret_value(SecretId=os.environ["SECRET_NAME"])["SecretString"])
 
-def lambda_handler(event, context):
+def lambda_handler(event: None, context: LambdaContext) -> None:
     datetime_utc = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S%z")
     random_int = random.randint(0, 1000)
     print(f"At {datetime_utc}, random integer: {random_int}; secret: {SECRET}")  # irl, wouldn't print secret
