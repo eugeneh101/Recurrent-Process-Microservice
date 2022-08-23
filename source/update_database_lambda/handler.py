52038e05acb13141a429016497e4d8747efa5967
Eugene <eugeneh101@DESKTOP-6EJMNGA.localdomain>
2022-08-23 12:55:21 -0700

    switched print() to logger.info()

--- source/update_database_lambda/handler.py
+++ source/update_database_lambda/handler.py
@@ -5,18 +5,27 @@ from datetime import datetime
 
 # from dotenv import load_dotenv
 import boto3
+from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 # load_dotenv()
+logger = Logger(service="my-service", level="INFO")
 dynamodb_table = boto3.resource("dynamodb").Table(os.environ["DYNAMODB_TABLE"])
 ssm = boto3.client("secretsmanager")
-SECRET = json.loads(ssm.get_secret_value(SecretId=os.environ["SECRET_NAME"])["SecretString"])
+SECRET = json.loads(
+    ssm.get_secret_value(SecretId=os.environ["SECRET_NAME"])["SecretString"]
+)
+
 
 def lambda_handler(event: None, context: LambdaContext) -> None:
     datetime_utc = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S%z")
     random_int = random.randint(0, 1000)
-    print(f"At {datetime_utc}, random integer: {random_int}; secret: {SECRET}")  # irl, wouldn't print secret
-    dynamodb_table.put_item(Item={"datetime_utc": datetime_utc, "random_int": random_int})
+    logger.info(
+        f"At {datetime_utc}, random integer: {random_int}; secret: {SECRET}"
+    )  # irl, wouldn't print secret
+    dynamodb_table.put_item(
+        Item={"datetime_utc": datetime_utc, "random_int": random_int}
+    )
     return
 
     # figure out how to get pyproject.toml -> requirements.txt -> Lambda Layer or Make install
