2da577c9139e9e81488951539461b8583a940aae
Eugene <eugeneh101@DESKTOP-6EJMNGA.localdomain>
2022-11-24 14:52:57 -0500

    start at the beginning of the minute

--- source/get_next_minute_and_invocation_times_lambda/handler.py
+++ source/get_next_minute_and_invocation_times_lambda/handler.py
@@ -9,9 +9,9 @@ def lambda_handler(event: None, context: LambdaContext) -> dict[str, Union[str, 
     now = datetime.utcnow()
     delta = timedelta(minutes=1)
     next_minute = (now + delta).replace(second=0, microsecond=0)
-    num_intervals = 60 // INTERVAL_IN_SECONDS
+    num_intervals = int(60 // INTERVAL_IN_SECONDS)
     invocation_times = []
-    for ith_interval in range(1, num_intervals + 1):
+    for ith_interval in range(num_intervals):
         invocation_time = next_minute + timedelta(seconds=INTERVAL_IN_SECONDS) * ith_interval
         invocation_times.append(invocation_time.strftime("%Y-%m-%dT%H:%M:%SZ"))
     return {
